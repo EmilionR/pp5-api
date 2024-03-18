b592c1341ab3a5427e87c6d01416019b43e4c877
Emilion <emiljadersten@gmail.com>
2024-03-18 16:44:45 +0100

    Add seralization for posts

---
+++ posts/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from posts.models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    """
+    Serialize posts and retrieve relevant data
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    # Check if current user is the owner
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Post
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
+            'friends_only', 'title', 'content', 'image',
+            'created_on', 'updated_on'
+        ]
