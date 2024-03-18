7ae977feb4e5c5da40c2ea7f3cf0270252f8d802
Emilion <emiljadersten@gmail.com>
2024-03-18 18:20:14 +0100

    Add Post list view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostList(APIView):
+    serializer_class = PostSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(
+            posts, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostSerializer(
+            data=request.data, context={'request': request}
+        )
+        # Check validity before creating post
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        # If invalid, return bad request
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
