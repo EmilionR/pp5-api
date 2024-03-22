64b0e124c84892d9425962183db8d3ab2616516b
Emilion <emiljadersten@gmail.com>
2024-03-22 16:54:50 +0100

    Serialize the Like model

--- likes/models.py
+++ likes/models.py
@@ -8,7 +8,7 @@ class Like(models.Model):
     Like model, pairing an 'owner' and a 'post', where:
     'owner' is a User instance
     'post' is a Post instance.
-    'unique_together' ensures that users can't like the same post twice
+    
     """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(
@@ -17,8 +17,11 @@ class Like(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
+        # Order likes by time of creation
         ordering = ['-created_on']
+        # 'unique_together' ensures that users can't like the same post twice
         unique_together = ['owner', 'post']
 
     def __str__(self):
         return f'{self.owner} {self.post}'
+    
