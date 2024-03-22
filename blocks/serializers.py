78a93f92217e8862cdc7c7d4c5d63685bee979ad
Emilion <emiljadersten@gmail.com>
2024-03-22 17:21:50 +0100

    Serialize the block model

---
+++ blocks/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from django.db import IntegrityError
+from .models import Block
+
+
+class BlockSerializer(serializers.ModelSerializer):
+    """
+    Serialize the Block model
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    # Create method that avoids duplicate blocks
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
+
+    class Meta:
+        model = Block
+        fields = ['id', 'owner', 'target', 'created_on']
