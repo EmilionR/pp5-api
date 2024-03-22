fd97f7d383da77ae2379d2daaadd803d35e39bc8
Emilion <emiljadersten@gmail.com>
2024-03-22 16:18:00 +0100

    Add comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
 
-# Create your models here.
+
+class Comment(models.Model):
+    """
+    Comment model, for comments related to a post
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    # Sort comments by time of creation in descending order
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.content
