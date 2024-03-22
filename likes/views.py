bd7bbadedbe256d9fe394856740d4d9a16316945
Emilion <emiljadersten@gmail.com>
2024-03-22 17:08:13 +0100

    Create like detail view

--- likes/views.py
+++ likes/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, permissions
 from likes.models import Like
 from likes.serializers import LikeSerializer
+from drf_api.permissions import IsOwnerOrReadOnly
 
 
 class LikeList(generics.ListCreateAPIView):
@@ -15,3 +16,12 @@ class LikeList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a like,
+    if you're the owner, also delete the like
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+    
