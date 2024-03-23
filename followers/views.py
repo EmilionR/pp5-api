f33b3d8db0db45707ead662915a095b0e81eec92
Emilion <emiljadersten@gmail.com>
2024-03-23 09:17:06 +0100

    Create views for followers

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Follower
+from .serializers import FollowerSerializer
+from drf_api.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    """
+    List all instances of User following another User
+    Create a follower-following pair if logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
+
+    # Associate the signed-in user with a follower.
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a follower
+    If owner, delete a follower pair to unfollow someone
+    """
+    queryset = Follower.objects.all()
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FollowerSerializer
