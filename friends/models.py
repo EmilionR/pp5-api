65c95f36971d8fccd56f1cad6928ff08de54e78c
Emilion <emiljadersten@gmail.com>
2024-03-23 09:25:01 +0100

    Create friend model

--- friends/models.py
+++ friends/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Friend(models.Model):
+    """
+    Friend model, for befriending other users
+    'owner' is a User instance, declaring another User a friend
+    'friend' is a User instance chosen as a friend by 'owner'
+    """
+    owner = models.ForeignKey(
+        User, related_name='following', on_delete=models.CASCADE
+    )
+    friend = models.ForeignKey(
+        User, related_name='friend', on_delete=models.CASCADE
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+        # prevent duplicate friend instances
+        unique_together = ['owner', 'friend']
+
+    def __str__(self):
+        return f'{self.owner}, {self.friend}'
+    
