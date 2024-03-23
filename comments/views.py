94c5e4a5a6e66b61996c8602414f9fb460086082
Emilion <emiljadersten@gmail.com>
2024-03-23 12:21:06 +0100

    Implement filtering for comments

--- comments/views.py
+++ comments/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics, permissions
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
-
+from django_filters.rest_framework import DjangoFilterBackend
 
 class CommentList(generics.ListCreateAPIView):
     """
@@ -10,6 +10,8 @@ class CommentList(generics.ListCreateAPIView):
     If logged in, create a comment
     """
     queryset = Comment.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['post']
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     
