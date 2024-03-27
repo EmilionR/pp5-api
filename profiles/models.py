ad5d641e1ad3e65c75b7f36283800bfdfdc533f3
Emilion <emiljadersten@gmail.com>
2024-03-27 10:57:20 +0100

    Change the default avatar

--- profiles/models.py
+++ profiles/models.py
@@ -17,7 +17,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_fnkrgd'
+        upload_to='images/', default='../default-avatar_vwnlto'
     )
 
     # Order profiles by time of creation in descending order
